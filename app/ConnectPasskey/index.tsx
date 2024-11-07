c3c29cf14848d16e83390e1074a1dbb1421adcf2
phamhugducBacoor <phamhungduc@bacoor.co>
2024-11-07 09:53:33 +0700

    Remove sdk-passkeyring dependency from package.json and clean up yarn.lock

--- app/ConnectPasskey/index.tsx
+++ app/ConnectPasskey/index.tsx
@@ -2,52 +2,56 @@ import React from "react";
 import { useConnect, useAccount } from "sdk-passkeyring";
 
 const ConnectPasskey = () => {
-  const { onConnect, isConnected, onDisconnect } = useConnect();
+  const { onConnect, isConnected, onDisconnect, onOpenWallet } = useConnect();
   const account = useAccount() || {};
   const { address } = account;
 
   return (
-    <div 
-    style={{
-      width: '100vw',
-      height: "100dvh",
-      display: 'flex',
-      alignItems: "center",
-      justifyContent: "center",
-    }}>
+    <div
+      style={{
+        width: "100vw",
+        height: "100dvh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div></div>
       {isConnected ? (
         <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: "center",
-          justifyContent: "center",
-          textAlign: 'center',
-          gap: '20px',
-        }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+            gap: "20px",
+          }}
         >
           <h1>Connected</h1>
           <div>
-              <button
-            onClick={() => {
-              onConnect();
-            }}
-          >
-            Address: {address}
-          </button>
+            <button
+              onClick={() => {
+                onConnect();
+              }}
+            >
+              Address: {address}
+            </button>
           </div>
-        
-      
+
           <div>
-              <button
-            onClick={() => {
-              onDisconnect();
-            }}
-          >
-            Disconnect
-          </button>
+            <button
+              onClick={() => {
+                onDisconnect();
+              }}
+            >
+              Disconnect
+            </button>
           </div>
-        
+
+          <div>
+            <button onClick={() => onOpenWallet("NFT")}>onOpenWallet</button>
+          </div>
         </div>
       ) : (
         <button onClick={onConnect}>Connect</button>
