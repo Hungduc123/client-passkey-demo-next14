1d60f5e479983d1c9804b8319fd8ab554851669b
phamhugducBacoor <phamhungduc@bacoor.co>
2024-12-23 12:01:09 +0700

    Comment out unused Wagmi configuration in app/config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,10 +1,15 @@
-import { http, createConfig } from 'wagmi'
-import { arbitrum, base, bsc, mainnet, optimism, polygon, sepolia } from 'wagmi/chains'
+// import { http, createConfig } from 'wagmi'
+// import { arbitrum, base, bsc, mainnet, optimism, polygon, sepolia } from 'wagmi/chains'
 
-export const config = createConfig({
-  chains: [mainnet, sepolia, polygon, arbitrum, optimism, base, bsc],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
-})
+// export const config = createConfig({
+//   chains: [mainnet, sepolia, polygon, arbitrum, optimism, base, bsc],
+//   transports: {
+//     [mainnet.id]: http(),
+//     [sepolia.id]: http(),
+//     [polygon.id]: http(),
+//     [arbitrum.id]: http(),
+//     [optimism.id]: http(),
+//     [base.id]: http(),
+//     [bsc.id]: http(),
+//   },
+// })
