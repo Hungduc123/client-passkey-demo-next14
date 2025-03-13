342d5e5f6c12ae08f570b2049d7f344f4325b7ce
phamhugducBacoor <phamhungduc@bacoor.co>
2025-03-13 11:48:00 +0700

    Refactor SignMessage component to use useAccount for address retrieval and improve logging

--- app/Components/SignMessage.tsx
+++ app/Components/SignMessage.tsx
@@ -3,25 +3,26 @@ import { signMessage } from "@wagmi/core";
 import { config, wagmiAdapter } from "@/context";
 import { verifyMessage } from "@ambire/signature-validator";
 import { ethers } from "ethers";
-import { getChainId } from '@wagmi/core'
+import { getChainId } from "@wagmi/core";
+import { base } from "viem/chains";
+import { useAccount } from "wagmi";
 const SignMessage = () => {
-  const chainId = getChainId(config)
-  console.log("🚀 ~ chainId:", chainId)
-  
+  const chainId = getChainId(config);
+  console.log("🚀 ~ chainId:", chainId);
+  // const a=base
+
   const message = "Hello world";
-  const address = "0x109105af554c00c2c419686c9e969e1fb2b6beba";
+  const account = useAccount();
+  const address = account?.address;
+  console.log("🚀 ~ address:", address)
 
-
-const listProvider: { [key: number]: string } = {
-  137: "https://rpc.ankr.com/polygon/23f34102610d22b6d3c81e37a40cb843717e15c4bb1f3ca04b2a3d9696e6988d",
-  42161: "https://rpc.ankr.com/arbitrum/23f34102610d22b6d3c81e37a40cb843717e15c4bb1f3ca04b2a3d9696e6988d",
-  8453: "https://rpc.ankr.com/base/23f34102610d22b6d3c81e37a40cb843717e15c4bb1f3ca04b2a3d9696e6988d",
-};
-  
-
-  const provider = new ethers.providers.JsonRpcProvider(
-    listProvider[chainId]
-  );
+  const listProvider: { [key: number]: string } = {
+    137: "https://rpc.ankr.com/polygon/23f34102610d22b6d3c81e37a40cb843717e15c4bb1f3ca04b2a3d9696e6988d",
+    42161: "https://rpc.ankr.com/arbitrum/23f34102610d22b6d3c81e37a40cb843717e15c4bb1f3ca04b2a3d9696e6988d",
+    8453: "https://rpc.ankr.com/base/23f34102610d22b6d3c81e37a40cb843717e15c4bb1f3ca04b2a3d9696e6988d",
+  };
+    
+  const provider = new ethers.providers.JsonRpcProvider(listProvider[chainId]);
   return (
     <div
       onClick={async () => {
@@ -29,7 +30,10 @@ const listProvider: { [key: number]: string } = {
           const result = await signMessage(wagmiAdapter.wagmiConfig, {
             message,
           });
-          console.log(" result:", result)
+          console.log("🚀 ~ chainId:", chainId);
+
+          console.log(" result:", result);
+          console.log("🚀 ~ onClick={ ~ provider:", provider);
 
           const isValidSig = await verifyMessage({
             signer: address,
